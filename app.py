e8a7ade72c2d9def60620c35cd3ae8eacb16358c
moshe-max <grossmoshe999@gmail.com>
2025-10-21 01:10:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import *
 from pytube import YouTube
 import re
 
@@ -76,4 +76,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
