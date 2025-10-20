6d2104d15b749f60769e4ae6d981efb295d477ee
moshe-max <grossmoshe999@gmail.com>
2025-10-21 01:06:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,7 @@ def video_info():
         return jsonify({"error": error_message}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
