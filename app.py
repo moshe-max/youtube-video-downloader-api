fa86e1992943f4c1c68104fcc4b1d061d8f17e6c
moshe-max <grossmoshe999@gmail.com>
2025-10-21 01:13:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import *
 from pytube import YouTube
 import re
+import os
 
 app = Flask(__name__)
 
@@ -76,5 +77,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
